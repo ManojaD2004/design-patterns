0d23c7420b6fddf8edd2bc12f48d217eab190067
ManojaD2004 <manojadkc2004@gmail.com>
2024-11-05 02:26:05 +0530

    Adapter done!

--- structural-patterns/adapter.ts
+++ structural-patterns/adapter.ts
@@ -1,25 +1,28 @@
 import chalk from "chalk";
 
-interface Product1 {
-  prdName: string;
-  prdPrice: number;
+// My Product Data Structure
+class Product1 {
+  constructor(public prdName: string, public prdPrice: number) {}
 }
 
-interface Product2 {
-  prdName: string;
-  details: {
-    prdPrices: {
-      default: number;
-      india?: number;
-      usa?: number;
-      china?: number;
-      russia?: number;
-    };
-  };
+// Friend Product Data Structure
+class Product2 {
+  constructor(
+    public prdName: string,
+    public details: {
+      prdPrices: {
+        default: number;
+        india?: number;
+        usa?: number;
+        china?: number;
+        russia?: number;
+      };
+    }
+  ) {}
 }
 
 interface Service<T extends Product1 | Product2> {
-  priceCheckService(prdData1: T, prdData2: T): void;
+  priceCheckService(prd1: T, prd2: T): void;
 }
 
 class ProductFactory {
@@ -39,88 +42,65 @@ class ProductFactory {
   }
 }
 class MyLogicCode implements Service<Product1> {
-  priceCheckService(prdData1: Product1, prdData2: Product1) {
-    if (prdData1.prdPrice === prdData2.prdPrice) {
+  priceCheckService(prd1: Product1, prd2: Product1) {
+    if (prd1.prdPrice === prd2.prdPrice) {
       console.log(
-        chalk.red(
-          `${prdData1.prdName} and ${prdData2.prdName} are the same price!`
-        )
+        chalk.red(`${prd1.prdName} and ${prd2.prdName} are the same price!`)
       );
-    } else if (prdData1.prdPrice > prdData2.prdPrice) {
+    } else if (prd1.prdPrice > prd2.prdPrice) {
       console.log(
-        chalk.red(
-          `${prdData1.prdName} is Greater than ${prdData2.prdName} in price!`
-        )
+        chalk.red(`${prd1.prdName} is Greater than ${prd2.prdName} in price!`)
       );
-    } else if (prdData1.prdPrice < prdData2.prdPrice) {
+    } else if (prd1.prdPrice < prd2.prdPrice) {
       console.log(
-        chalk.red(
-          `${prdData1.prdName} is Lesser than ${prdData2.prdName} in price!`
-        )
+        chalk.red(`${prd1.prdName} is Lesser than ${prd2.prdName} in price!`)
       );
     }
   }
 }
 
-class MyFriendLogicCode implements Service<Product2> {
-  priceCheckService(prdData1: Product2, prdData2: Product2) {
-    if (
-      prdData1.details.prdPrices.default === prdData2.details.prdPrices.default
-    ) {
-      console.log(
-        chalk.red(
-          `${prdData1.prdName} and ${prdData2.prdName} are the same price!`
-        )
-      );
-    } else if (
-      prdData1.details.prdPrices.default > prdData2.details.prdPrices.default
-    ) {
-      console.log(
-        chalk.red(
-          `${prdData1.prdName} is Greater than ${prdData2.prdName} in price!`
-        )
-      );
-    } else if (
-      prdData1.details.prdPrices.default < prdData2.details.prdPrices.default
-    ) {
-      console.log(
-        chalk.red(
-          `${prdData1.prdName} is Lesser than ${prdData2.prdName} in price!`
-        )
-      );
-    }
-  }
-}
-
-class ProductAdapter implements Service<Product1 | Product2> {
-  adaptee: Service<Product1 | Product2>;
-  constructor(adaptee: Service<Product1 | Product2>) {
+class Product2Adapter extends Product1 {
+  private adaptee: Product2;
+  constructor(adaptee: Product2) {
+    super(adaptee.prdName, adaptee.details.prdPrices.default);
     this.adaptee = adaptee;
-  }
-  priceCheckService(
-    prdData1: Product1 | Product2,
-    prdData2: Product1 | Product2
-  ) {
-    if (this.isProduct1(prdData1) === true) {
-        
-    }
-  }
-  isProduct1(prd: Product1 | Product2): prd is Product1 {
-    if ((prd as Product1).prdPrice !== undefined) {
-      return true;
-    } else {
-      return false;
-    }
+    // The adaptee can be used in any other methods you want!
   }
 }
 
-function clientLogic() {}
+function clientLogic(
+  serviceLogic: MyLogicCode,
+  prd1: Product1 | Product2Adapter,
+  prd2: Product1 | Product2Adapter
+) {
+  serviceLogic.priceCheckService(prd1, prd2);
+}
 
 function main() {
   console.log(chalk.yellow("Adapter:"));
   //   Better example would be to use Adapter for JSON to XML/YAML, vice versa
-  const n = 5;
-  clientLogic();
+  console.log(
+    chalk.blue("Creating 2 different products with 2 different types...")
+  );
+  const prdFact = new ProductFactory();
+  const prd1T1 = prdFact.createProduct1("IPhone X", 1000);
+  const prd2T1 = prdFact.createProduct1("Dell Ultra", 2000);
+  const prd1T2 = prdFact.createProduct2("IPhone X", {
+    prdPrices: { default: 1000 },
+  });
+  const prd2T2 = prdFact.createProduct2("Dell Ultra", {
+    prdPrices: { default: 2000 },
+  });
+  const prd1AT2 = new Product2Adapter(prd1T2);
+  const prd2AT2 = new Product2Adapter(prd2T2);
+  const serviceLogic = new MyLogicCode();
+  clientLogic(serviceLogic, prd1T1, prd2T1);
+  clientLogic(serviceLogic, prd1T1, prd2AT2);
+  clientLogic(serviceLogic, prd1AT2, prd2T1);
+  clientLogic(serviceLogic, prd1AT2, prd2AT2);
+  console.log(
+    chalk.blue("Adapter makes two different types/data/code to be compatible")
+  );
 }
 
 export { main };
